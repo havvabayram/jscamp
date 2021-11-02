febff237eb8176d759602b36a3d4e70f96e281b9
= <havvabayram09@gmail.com>
2021-11-02 13:55:59 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("Merhaba kodlama i.o")
+
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 //bu yapılamıyo
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+
+console.log(konutKredileri)
